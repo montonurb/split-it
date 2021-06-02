4cc5fb476bd7d0c846dc2c0b23a757a08159f180
Bruno Monteiro <brunomoncar007@gmail.com>
2021-06-02 20:47:27 -0300

    adicionando dados na tela home e trocando a forma do navigator

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:split_it/modules/login/widgets/models/user_model.dart';
 import 'package:split_it/theme/app_theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final UserModel user =
+        ModalRoute.of(context)!.settings.arguments as UserModel;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("${user.name}"),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.network("${user.photoUrl}"),
+        ),
+        centerTitle: true,
         backgroundColor: AppTheme.colors.backgroundSecondary,
       ),
     );
