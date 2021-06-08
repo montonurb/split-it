b4d2bca2bd9b0c1ffcf0fb379ab2f20fb2159e0a
Bruno Monteiro <brunomoncar007@gmail.com>
2021-06-08 16:44:06 -0300

    adicionando padding no list

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/widgets/app_bar_widget.dart';
-import 'package:split_it/modules/home/widgets/info_card_widget.dart';
 import 'package:split_it/modules/home/widgets/list_history_widget.dart';
 import 'package:split_it/modules/login/widgets/models/user_model.dart';
 
@@ -21,12 +20,20 @@ class _HomePageState extends State<HomePage> {
           print("Clicou!");
         },
       ),
-      body: Column(
-        children: [
-          ListHistoryWidget(value: -350),
-          ListHistoryWidget(value: 350),
-          ListHistoryWidget(value: -350),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListHistoryWidget(value: -350),
+              ListHistoryWidget(value: 350),
+              ListHistoryWidget(value: -350),
+              ListHistoryWidget(value: -350),
+              ListHistoryWidget(value: -350),
+              ListHistoryWidget(value: -350),
+            ],
+          ),
+        ),
       ),
     );
   }
